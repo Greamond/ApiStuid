21fd4d4df5b62dc06f80878afdc75da458363c36
Rusanov <Nikolay.Rusan0v@yandex.ru>
2025-05-23 21:44:57 +0500

    Добавление задач. Русанов

--- ApiStuid/Controllers/TasksController.cs
+++ ApiStuid/Controllers/TasksController.cs
@@ -1,10 +1,12 @@
 ﻿using ApiStuid.DbWork;
+using ApiStuid.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ModelsTask = ApiStuid.Models.Task;
+using ApiStuid.Models;
 
 namespace ApiStuid.Controllers
 {
@@ -19,86 +21,72 @@ namespace ApiStuid.Controllers
             _context = context;
         }
 
-        // GET: api/Tasks
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ModelsTask>>> GetTasks()
-        {
-            return await _context.Tasks.ToListAsync();
-        }
-
         // GET: api/Tasks/5
         [HttpGet("{id}")]
         public async Task<ActionResult<ModelsTask>> GetTask(int id)
         {
-            var task = await _context.Tasks.FirstOrDefaultAsync(t => t.Id == id);
+            var task = await _context.Tasks.FindAsync(id);
 
             if (task == null)
             {
                 return NotFound();
             }
+
+            // Загружаем ответственных за задачу
+            var responsibles = await _context.TaskResponsibles
+                .Where(tr => tr.TaskId == id)
+                .ToListAsync();
 
             return task;
         }
 
-        // POST: api/Tasks
-        [HttpPost]
-        public async Task<ActionResult<ModelsTask>> PostTask(ModelsTask task)
+        // GET: api/Tasks/project/5
+        [HttpGet("project/{projectId}")]
+        public async Task<ActionResult<IEnumerable<ModelsTask>>> GetTasksByProject(int projectId)
         {
+            return await _context.Tasks
+                .Where(t => t.ProjectId == projectId)
+                .ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ModelsTask>> PostTask([FromBody] TaskCreateRequest request)
+        {
+            var task = new ModelsTask
+            {
+                Name = request.Name,
+                Description = request.Description,
+                ProjectId = request.ProjectId,
+                Chapter = 1
+            };
+
             _context.Tasks.Add(task);
             await _context.SaveChangesAsync();
+
+            // Добавляем ответственных
+            if (request.AssigneeIds != null)
+            {
+                foreach (var assigneeId in request.AssigneeIds)
+                {
+                    _context.TaskResponsibles.Add(new TaskResponsible
+                    {
+                        TaskId = task.Id,
+                        UserId = assigneeId
+                    });
+                }
+                await _context.SaveChangesAsync();
+            }
 
             return CreatedAtAction(nameof(GetTask), new { id = task.Id }, task);
         }
 
-        // PUT: api/Tasks/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTask(int id, Task task)
+        public class TaskCreateRequest
         {
-            if (id != task.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(task).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TaskExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // DELETE: api/Tasks/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTask(int id)
-        {
-            var task = await _context.Tasks.FindAsync(id);
-            if (task == null)
-            {
-                return NotFound();
-            }
-
-            _context.Tasks.Remove(task);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool TaskExists(int id)
-        {
-            return _context.Tasks.Any(e => e.Id == id);
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public int ProjectId { get; set; }
+            public int ChapterId { get; set; }
+            public List<int> AssigneeIds { get; set; }
         }
     }
 }
--- ApiStuid/Models/Task.cs
+++ ApiStuid/Models/Task.cs
@@ -6,10 +6,13 @@ namespace ApiStuid.Models
     public class Task
     {
         public int Id { get; set; }
+
         [Column("project_id")]
         public int ProjectId { get; set; }
+
         public string Name { get; set; }
         public string Description { get; set; }
+
         [Column("chapter_id")]
         public int Chapter { get; set; }
     }
